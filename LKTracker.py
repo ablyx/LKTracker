6cebf4135d9354c1e91d676f1bd02f7f946202ce
Jonathan Lim Siu Chi <jonathan.lim@u.nus.edu>
2017-11-11 15:59:50 +0800

    Some changes

--- LKTracker.py
+++ LKTracker.py
@@ -84,15 +84,18 @@ def test_get_features():
     for y,x in features:
         cv2.circle(color_frame, (y, x), 1, (0, 0, 255), -1)
     cv2.imwrite('FEATURES200.jpg', color_frame)
-test_get_features()
+# test_get_features()
 
 
 # frames are grayscale and of the same size
-def LKTracker(frame, next_frame, corners=200):
+# should return centers of tracked features for better(larger) resolution of next_frame
+# returns the next next_frame_features
+def LKTracker(frame, next_frame, frame_features, next_frame_features):
     frame = frame.astype('int16')
     next_frame = next_frame.astype('int16')
     rows, cols = frame.shape[:2]
     print(rows, cols)
+
 
     # getting the derivatives
     # Ix and Iy store the gx and gy (derivatives) values
@@ -113,80 +116,70 @@ def LKTracker(frame, next_frame, corners=200):
     Iyy = np.multiply(gy, gy)
 
     # get Zd = b
-    # using a window of all ones instead of gaussian for speed (and convenience)
-    window = np.ones((13, 13))
-    # convolve gradients with window of interest
-    # these W matrices are used to get Z
-    Wxx = convolve2d(Ixx, window, mode='full')
-    Wxy = convolve2d(Ixy, window, mode='full')
-    Wyy = convolve2d(Iyy, window, mode='full')
-    print(Ixx.shape)
-    print(Wxx.shape)
 
     # getting b
     I = copy.deepcopy(frame)
     J = copy.deepcopy(next_frame)
-    bx = convolve2d(np.multiply((I - J), gx), window, mode='full')
-    by = convolve2d(np.multiply((I - J), gy), window, mode='full')
 
-    # initial d
-    dx = np.zeros((row, col))
-    dy = np.zeros((row, col))
-    # each pixel in the image has a corresponding window which gives matrix Z
-    # getting the smallest eigenvalue of Z
-    print('get eig')
-    wrows, wcols = Wyy.shape
-    eigvals = np.zeros((wrows, wcols))
-    for row in range(wrows):
-        for col in range(wcols):
-            W = np.matrix([[Wxx[row][col], Wxy[row][col]], [Wxy[row][col], Wyy[row][col]]])
-            vals, vec = np.linalg.eig(W)
-            eigvals[row][col] = min(vals)
+    larger_reso_next_frame_features = []
+    # feature is in the form (y,x)
+    for i, prev_feature in enumerate(frame_features):
+        prev_y, prev_x = prev_feature
+        Wxx = sum(Ixx[prev_y - 6: prev_y + 7, prev_x - 6: prev_x + 7].ravel())
+        Wxy = sum(Ixy[prev_y - 6: prev_y + 7, prev_x - 6: prev_x + 7].ravel())
+        Wyy = sum(Iyy[prev_y - 6: prev_y + 7, prev_x - 6: prev_x + 7].ravel())
 
+        Z = np.matrix([[Wxx, Wyy], [Wxy, Wyy]])
 
-    # split the image into 13 by 13 windows
-    # for each window, take the highest eigenvalue and set the rest to 0
-    # identify which pixels are good features
-    print('get mosaic eig')
-
-    for row in range(0, wrows-13, 13):
-        for col in range(0, wcols-13, 13):
-            window = eigvals[row:row+13, col:col+13]
-            largest_eig = max(window.ravel())
-            window = map(lambda row: map(lambda x: 0 if x != largest_eig else x, row), window)
-            eigvals[row:row+13, col:col+13] = window
-
-    # only track (get direction/movement for) good features
-    # only solve Zd = b when smallest eigenvalue of Z is large
-    # this eigenvalue has already been obtained
-
-    print('get direction')
-    for x in range(w):
-        for y in range(h):
-            if eigvals[y][x] != 0:
-                Z = np.matrix([[Wxx[y][x], Wxy[y][x]], [Wxy[y][x], Wyy[y][x]]])
-                try:
-                    # solve for d
-                    Z_inv = np.linalg.inv(Z)
-                    b = np.matrix([[bx[y][x],],[by[y][x]],])
-                    dxx, dyy = np.dot(Z_inv, b)
-                    dx[y][x] = dxx
-                    dx[y][x] = dyy
-                except:
-                    continue
-
-    return dx, dy, eigvals
+        prev_feature_window = I[prev_y-6: prev_y+7, prev_x-6: prev_x+7]
+        next_y, next_x = next_frame_features[i]
+        next_feature_window = J[next_y - 6: next_y + 7, next_x - 6: next_x + 7]
+        # w(I-J)
+        window_diff = prev_feature_window - next_feature_window
+        bx = sum(np.multiply(window_diff, gx).ravel())
+        by = sum(np.multiply(window_diff, gy).ravel())
+        try:
+            # solve for d
+            Z_inv = np.linalg.inv(Z)
+            b = np.matrix([bx, by])
+            dx, dy = np.dot(Z_inv, b)
+        except:
+            print('error solving Zd = b')
+            dx, dy = 0, 0
+        larger_reso_next_frame_features.append((next_y+dy, next_x+dx))
+    return larger_reso_next_frame_features
 
 # lets see if my code works
 # get the first 2 frames
-cap = cv2.VideoCapture('test/clip.mp4')
+cap = cv2.VideoCapture('source.mp4')
 ret, frame = cap.read()
 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+frame1_features = get_features(frame)
+
 ret, next_frame = cap.read()
 next_frame = cv2.cvtColor(next_frame, cv2.COLOR_BGR2GRAY)
+frame2_features = frame1_features
+result = LKTracker(frame, next_frame, frame1_features, frame2_features)
+
+## Draw circles of LKTracker result on next_frame
+color_frame = cv2.cvtColor(next_frame, cv2.COLOR_GRAY2RGB)
+
 # eigvals are gotten from the first frame
 # correspond to good features
 cv2.imwrite('test.jpg', frame)
+
+## Testing LKTracker
+##
+result = LKTracker
+
+def test_get_features():
+    frame = cv2.imread('test.jpg', 0)
+    features = get_features(frame)
+    color_frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+    print(features)
+    for y,x in features:
+        cv2.circle(color_frame, (y, x), 1, (0, 0, 255), -1)
+    cv2.imwrite('FEATURES200.jpg', color_frame)
 
 # dx, dy, eigvals = LKTracker(frame, next_frame)
 # print('track done, plot corners')
